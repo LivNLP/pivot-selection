f1133f12f639ea62d1b295ca1bcc5eb6b60bb72c
summer1278 <cxcui188@gmail.com>
2016-08-25 12:29:45 +0100

    add collect_features and save/load_objects

--- src/landmark_pivot.py
+++ src/landmark_pivot.py
@@ -1,6 +1,8 @@
 # landmark-based pivot selection method
 import select_pivots as sp 
 import numpy
+import pickle
+import glob
 import gensim,logging
 from glove import Corpus
 import glove
@@ -22,10 +24,13 @@ def review_list(fname):
 
 # word embedding: from word to word vector
 # Word2Vec
-def word2vec(domain_name):
+def word2vec_model(domain_name):
     model = gensim.models.Word2Vec(labeled_reviews(domain_name), min_count=1,workers=4)
     model.save('../work/%s/word2vec.model' % domain_name) 
     return model
+
+def word2vec(feature):
+    return 
 
 # GloVe
 def glo2ve(domain_name):
@@ -51,6 +56,63 @@ def ppmi(pmi_score):
 def df_diff(df_source,src_reviews,df_target,tgt_reviews):
     return df_source/src_reviews - df_target/tgt_reviews
 
+def df_function(source,target):
+    print 'loading objects...'
+    df_source = load_grouped_obj(source,target,'x_src')
+    df_target = load_grouped_obj(source,target,'x_un_tgt')
+    src_reviews = load_grouped_obj(source,target,'src_reviews')
+    tgt_reviews = load_grouped_obj(source,target,'un_tgt_reviews')
+    features = load_grouped_obj(source,target,'sl_tu_features')
+
+    print 'calculating...'
+
+    print ''
+    pass
+
+def load_grouped_obj(source,target,name):
+    with open("../../group-generation/%s-%s/obj/%s.pkl" % (source,target,name), 'rb') as f:
+        return pickle.load(f)
+
+def save_loop_obj(obj,dirname,name):
+    with open(dirname+"%s.pkl" % name,'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def save_grouped_obj(obj,source,target,name):
+    with open("../../group-generation/%s-%s/obj/%s.pkl" % (source,target,name),'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+# preset methods
+def collect_source_featrues():
+    domains = ["books", "electronics", "dvd", "kitchen"]
+    for domain in domains:
+        pos_src_features = features_list("../data/%s/train.positive" % domain)
+        neg_src_features = features_list("../data/%s/train.negative" % domain)
+        src_features = set(pos_src_features).union(set(neg_src_features))
+        dirnames = glob.glob('../../group-generation/%s-*'%domain)
+        for dirname in dirnames:
+            print 'saving src_featrues in '+ dirname + '...'
+            save_loop_obj(src_features,dirname,'src_features')   
+    print 'Complete!!'
+    pass
+
+def collect_features():
+    domains = ["books", "electronics", "dvd", "kitchen"]
+    for source in domains:
+        for target in domains:
+            if source ==target:
+                continue
+            pos_src_features = sp.features_list("../data/%s/train.positive" % source)
+            neg_src_features = sp.features_list("../data/%s/train.negative" % source)
+            src_features = set(pos_src_features).union(set(neg_src_features))
+            un_tgt_features = sp.features_list("../data/%s/train.unlabeled" % target)
+            sl_tu_features = src_features.union(set(un_tgt_features))
+
+            print 'saving sl_tu_features for %s-%s ...' % (source,target)
+            save_grouped_obj(sl_tu_features,source,target,'sl_tu_features')
+    pass
+
+
 # main
 if __name__ == "__main__":
-    domain_name = 'books'
+    # collect_features()
+    
