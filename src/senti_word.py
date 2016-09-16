19684d54208efab1d2126c5cf577100dcadb59b6
summer1278 <cxcui188@gmail.com>
2016-09-16 04:15:17 +0100

    add bigram support on sentiwordnet

--- src/senti_word.py
+++ src/senti_word.py
@@ -6,9 +6,12 @@ from nltk.corpus import sentiwordnet as swn
 # sentiwordnet score
 # sum up all the scores and take the average
 def senti_score(feature):
-    temp = swn.senti_synsets(feature)
-    return numpy.mean([x.pos_score()-x.neg_score() for x in temp]) if temp else 0
-
+    score = 0
+    for t in feature.split('__'):
+        temp = swn.senti_synsets(t)
+        score+=numpy.mean([x.pos_score()-x.neg_score() for x in temp]) if temp else 0
+    return score
+    
 # return number of neutral,pos,neg within a list
 def senti_list(feats):
     mid = sum(1 for x in feats if senti_score(x) == 0 )
@@ -20,5 +23,5 @@ def senti_list(feats):
 
 # main
 if __name__ == "__main__":
-    feats = ['happy','what','word','bad']
+    feats = ['happy','what','very__disappointed','bad']
     senti_list(feats)
