52f02433c118a29943f08b99dde6ad698846760d
summer1278 <cxcui188@gmail.com>
2016-10-23 14:06:46 +0100

    update

--- src/draw_sentiword.py
+++ src/draw_sentiword.py
@@ -163,7 +163,8 @@ def draw_params(method):
 if __name__ == "__main__":
     # method = "landmark_pretrained_word2vec"
     method = "landmark_pretrained_glove"
-    lookfor_pair = "K-E"
+    # lookfor_pair = "K-E"
+    lookfor_pair = "B-D"
     methods = ["landmark_pretrained_word2vec","landmark_pretrained_glove"]
     methods += ["freq","un_freq","mi","un_mi","pmi","un_pmi","ppmi","un_ppmi"]
     # draw_params(method)
