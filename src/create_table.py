f764d853b7dbaab122c1afd2f216b0715cddfe5a
summer1278 <cxcui188@gmail.com>
2016-10-31 16:05:36 +0000

    add table representation for SCL S,T or T only

--- src/create_table.py
+++ src/create_table.py
@@ -41,6 +41,24 @@ def collect_params(da_method,pv_method):
     return new_list
     pass
 
+def collect_params_on_server(da_method,pv_method):
+    new_list = []
+    input_file = open("../work/temp/%sparams.%s.csv"%(da_method,pv_method),'r')
+    next(input_file)
+    for line in input_file:
+        p = line.strip('\n').split(', ')
+        src = p[0][0].capitalize()
+        tgt = p[1][0].capitalize()
+        domain_pair = "%s-%s"%(src,tgt)
+        method = pv_method
+        acc = float(p[3])*100
+        interval = (float(p[5]) - float(p[4]))*100/2.0
+        param = '%.1f' % float(p[6]) if (float(p[6])>0.1 or float(p[6])==0) else '%.1e'%Decimal(p[6])
+        new_list.append((domain_pair,method, acc,interval,param))
+    return new_list
+    pass
+
+
 # convert names
 def convert(method):
     if "landmark_" in method:
@@ -112,12 +130,45 @@ def construct_accuracy_table(pv_methods,da_method):
     # print tabulate(table,headers,tablefmt="latex")
     pass
 
-# def construct_sentiword_table(pv_methods,da_method):
-#     pass
+def construct_SCL_table(pv_methods):
+    table = []
+    i = 0
+    #collect domain pairs
+    for method in pv_methods:
+        if i == 0:
+            m_list = collect_accuracy("../work/batchSCL.%s.csv"%method)
+            pairs = [x[0] for x in m_list]
+        i+=1
+
+    # use pairs to collect accuracies
+    for pair in pairs:
+        tmp = []
+        tmp.append(pair)
+        for method in pv_methods:
+            if "landmark" in method:
+                m_list = construct_best_landmark(collect_params('SCL',method))
+                tmp.append([x[2] for x in m_list if x[0]==pair][0])
+                m_list = construct_best_landmark(collect_params_on_server('SCL',method))
+                tmp.append([x[2] for x in m_list if x[0]==pair][0])
+            else:
+                m_list = collect_accuracy("../work/batch%s.%s.csv"% ('SCL', method))
+                tmp.append([x[2] for x in m_list if x[0]==pair][0])
+                m_list = collect_accuracy("../work/temp/batch%s.%s.csv"% ('SCL', method))
+                tmp.append([x[2] for x in m_list if x[0]==pair][0])
+        best = max(tmp[1:])
+        best_idx = [i for i, j in enumerate(tmp) if j == best]
+        new_tmp = ["\\textbf{%.2f}"%x if x == best else x for x in tmp]
+        # print pair,[convert(pv_methods[i-1]) for i in best_idx],best
+        table.append(new_tmp)
+
+    headers = ['S-T']+sum([[convert(x)+'(S,T)',convert(x)+'(T)'] for x in pv_methods],[])
+    print tabulate(table,headers,floatfmt=".2f")
+    pass
 
 if __name__ == "__main__":
-    methods = ["freq","un_freq","mi","un_mi","pmi","un_pmi","ppmi","un_ppmi"]
-    methods += ["landmark_pretrained_word2vec","landmark_pretrained_glove"]
+    # methods = ["freq","un_freq","mi","un_mi","pmi","un_pmi","ppmi","un_ppmi"]
+    methods = ["landmark_pretrained_word2vec","landmark_pretrained_glove"]
     DAmethod = "SCL"
     # DAmethod = "SFA"
-    construct_accuracy_table(methods,DAmethod)
+    # construct_accuracy_table(methods,DAmethod)
+    construct_SCL_table(methods)
