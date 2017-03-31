f5a4d4ac9e81c17dc6f987c5abdd36f4d56f3a68
summer1278 <cxcui188@gmail.com>
2017-03-31 13:12:28 +0100

    get embedding from wiki-ppmi

--- src/wiki_ppmi.py
+++ src/wiki_ppmi.py
@@ -3,25 +3,51 @@ import numpy
 # return a list of ids that has top 2000 frequencies
 # ids from ids ( no stop words in the list )
 # freq from unigrams.sorted ( useded to determine choose which 2000 )
-# ppmi from wik_ppmi ( use the ids_list genereated )
+# ppmi from wik-ppmi ( use the ids_list genereated )
 def select_top_k_words(k):
     id_file = open("../data/xia_data/ids","r")
     freq_file = open("../data/xia_data/unigrams.sorted","r")
     id_list = []
-    l1 = [line.strip.split()[1] for line in id_file]
-    print l1
-    l2 = [line.strip.split()[0] for line in freq_file]
-    pirnt l2
-    non_stop_words = list(set(l1) & set(l2))
-    word_list = [word for word in l2 if word in non_stop_words][:k]
-    for word in word_list:
-        id_list.append(get_id(word))
-    print id_list
-    return id_list
-def get_id(word):
-    return [line.strip.split()[0] for line in id_file if line.strip.split()[0]==word][0]
+    l1 = {}
+    # word_list = []
+    for line in id_file:
+        l1[line.split()[1]]=int(line.split()[0])
+    # print len(l1)
+    l2 = [line.split()[0] for line in freq_file]
+    # print len(l2)
 
+    for x in l2:
+        if x in frozenset(l1.keys()):
+            if len(id_list) < k:
+                id_list.append(l1.get(x))
+            else:
+                break
+                pass
+    # print id_list
+    return id_list
+
+# get ppmi values
+def ppmi_embedding_model(id_list):
+    ppmi_file = open("../data/xia_data/wiki-ppmi","r")
+    model = {}
+    print id_list
+    for line in ppmi_file:
+        splitLine = line.split()
+        word = splitLine[0]
+        embedding = []
+        for val in splitLine[1:]:
+            p = val.split(':')
+            # print p
+            if int(p[0]) in id_list:
+                embedding.append(float(p[1]))
+            elif len(embedding)<len(id_list):
+                embedding.append(0.0)
+            # print len(embedding)
+        model[word] = embedding
+        # print word,embedding
+    print len(model)," words loaded!",len(ppmi_file)==len(model)
+    return model
 
 # main
 if __name__ == "__main__":
-    select_top_k_words(10)
+    ppmi_embedding_model(select_top_k_words(10))
